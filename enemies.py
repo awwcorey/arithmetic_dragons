0109781403a38ac5e687095a1a4686ae2fb7c2d2
awwcorey <awwcorey@net123.ipoe-users.mipt.ru>
2016-11-17 14:25:39 +0300

    есть дракоши и тролль

--- enemies.py
+++ enemies.py
@@ -39,8 +39,46 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 20
+        self._color = 'красный'
+
+    def question(self):
+        x = randint(1, 100)
+        y = randint(1, 100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 25
+        self._color = 'черный'
+
+    def question(self):
+        x = randint(1, 100)
+        y = randint(1, 100)
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x * y)
+        return self.__quest
+
+class Troll(Dragon):
+    def __init__(self):
+        self._health = 100
+        self._attack = 25
+        self._color = 'черный'
+
+    def question(self):
+        x = randint(1, 5)
+        self.__quest = 'Угадайте число от 1 до 5'
+        self.set_answer(x)
+        return self.__quest
+
+        #FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
 # красный дракон учит вычитанию, а чёрный -- умножению.
 
 
-enemy_types = [GreenDragon, RedDragon, BlackDragon]
+enemy_types = [GreenDragon, RedDragon, BlackDragon, Troll]
